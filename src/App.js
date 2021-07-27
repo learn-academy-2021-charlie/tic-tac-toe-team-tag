1c6bd6fb056ea9d89fe0ca9beca3f1cb6c184fac
ArtyG-10 <a.gourent@outlook.com>
2021-07-27 19:07:11 -0400

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -26,9 +26,7 @@ class App extends Component{
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (this.state.squares[a] && this.state.squares[a] === this.state.squares[b] && this.state.squares[a] === this.state.squares[c]) {
-        console.log(this.state.activePlayer)
         this.setState({winingPlayer: this.state.squares[a]})
-        
       }
     }
     return null;
@@ -37,15 +35,17 @@ class App extends Component{
   handleGamePlay = (index) => {
     const {squares, activePlayer} = this.state
     if (activePlayer === "player1") {
-      squares[index] = "X"
+      squares[index] = "😇"
       this.setState({squares: squares, activePlayer: "player2"})
+      this.calculateWinner()
     }
     else if (activePlayer === "player2") {
-      squares[index] = "O"
+      squares[index] = "👹"
       this.setState({squares: squares, activePlayer: "player1"})
+      this.calculateWinner()
     }
   }
-  
+
 
   render(){
     return(
@@ -63,7 +63,7 @@ class App extends Component{
         )
       })}
     </div>
-    <p>Player {this.state.winingPlayer} you have won</p>
+    {this.state.winingPlayer && <p id="win">Player {this.state.winingPlayer} you are the winner!</p>}
       </>
     )
   }
